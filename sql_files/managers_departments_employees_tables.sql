44e7cbdcf53453dcd31ac9f6489c09f546fe6bfd
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-15 14:49:23 +0300

    Added sql file

--- sql_files/managers_departments_employees_tables.sql
+++ sql_files/managers_departments_employees_tables.sql
@@ -25,12 +25,32 @@ CREATE TABLE
 CREATE TABLE
     employees(
         id INTEGER GENERATED ALWAYS AS IDENTITY ( START WITH 10001 INCREMENT 1 ) PRIMARY KEY,
-        manager_id INTEGER,
         department_id INTEGER,
         first_name VARCHAR(30),
         last_name VARCHAR(30),
         email VARCHAR(30),
         phone_number VARCHAR(20),
-        employed_at DATE
+        employed_at DATE,
+        
+        CONSTRAINT fk_employees_departments
+             FOREIGN KEY (department_id)
+             REFERENCES departments(id)
+             ON UPDATE CASCADE 
+             ON DELETE CASCADE 
+);
+
+CREATE TABLE 
+    activities_record(
+        id SERIAL PRIMARY KEY,
+        employee_id INTEGER NOT NULL,
+        type_of_activity VARCHAR(6) NOT NULL,
+        description VARCHAR(200) NOT NULL,
+        date_and_time TIMESTAMPTZ,
+        
+        CONSTRAINT fk_activities_record_employees
+                     FOREIGN KEY (employee_id)
+                     REFERENCES employees(id)
+                     ON UPDATE CASCADE 
+                     ON DELETE CASCADE 
 );
 
