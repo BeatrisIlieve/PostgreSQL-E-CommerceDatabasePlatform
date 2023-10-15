09b5ba2f2a90cb4b2db1b6a081174e6f42c6c80b
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-15 07:45:43 +0300

    Added sql file

---
+++ sql_files/earrings_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE earrings(
+    id SERIAL PRIMARY KEY,
+    image_url VARCHAR(200),
+    earring_name VARCHAR(100),
+    price DECIMAL(7, 2),
+    metal_color VARCHAR(12),
+    diamond_carat_weight VARCHAR(10),
+    diamond_clarity VARCHAR(10),
+    diamond_color VARCHAR(5),
+    description TEXT
+);
