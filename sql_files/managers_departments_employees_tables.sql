e148ebc63e952a1af455a543c20127a401630cb2
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-15 16:23:49 +0300

    Added sql file

--- sql_files/managers_departments_employees_tables.sql
+++ sql_files/managers_departments_employees_tables.sql
@@ -8,6 +8,11 @@ CREATE TABLE
         employed_at DATE
 );
 
+insert into managers (first_name, last_name, email, phone_number, employed_at) values ('Bab', 'Delagua', 'bdelagua1@buzzfeed.com', '805-186-8739', '9/5/2023');
+insert into managers (first_name, last_name, email, phone_number, employed_at) values ('Rubia', 'Franssen', 'rfranssen0@umich.edu', '638-526-2342', '8/3/2023');
+insert into managers (first_name, last_name, email, phone_number, employed_at) values ('Rem', 'Cordell', 'rcordell3@tuttocitta.it', '522-523-4027', '7/16/2023');
+insert into managers (first_name, last_name, email, phone_number, employed_at) values ('Danice', 'Dunne', 'ddunne4@noaa.gov', '922-710-6398', '10/16/2022');
+insert into managers (first_name, last_name, email, phone_number, employed_at) values ('Grenville', 'Addie', 'gaddie6@ehow.com', '809-184-2377', '9/3/2023');
 
 CREATE TABLE
     departments(
@@ -22,6 +27,12 @@ CREATE TABLE
                ON DELETE CASCADE
 );
 
+insert into departments (manager_id, department_name) values (30001, 'Merchandising');
+insert into departments (manager_id, department_name) values (30002, 'Purchasing');
+insert into departments (manager_id, department_name) values (30003, 'Shipping');
+insert into departments (manager_id, department_name) values (30004, 'Quality Control');
+insert into departments (manager_id, department_name) values (30005, 'Customer Service');
+
 CREATE TABLE
     employees(
         id INTEGER GENERATED ALWAYS AS IDENTITY ( START WITH 10001 INCREMENT 1 ) PRIMARY KEY,
@@ -31,26 +42,16 @@ CREATE TABLE
         email VARCHAR(30),
         phone_number VARCHAR(20),
         employed_at DATE,
-        
+
         CONSTRAINT fk_employees_departments
              FOREIGN KEY (department_id)
              REFERENCES departments(id)
-             ON UPDATE CASCADE 
-             ON DELETE CASCADE 
+             ON UPDATE CASCADE
+             ON DELETE CASCADE
 );
 
-CREATE TABLE 
-    activities_record(
-        id SERIAL PRIMARY KEY,
-        employee_id INTEGER NOT NULL,
-        type_of_activity VARCHAR(6) NOT NULL,
-        description VARCHAR(200) NOT NULL,
-        date_and_time TIMESTAMPTZ,
-        
-        CONSTRAINT fk_activities_record_employees
-                     FOREIGN KEY (employee_id)
-                     REFERENCES employees(id)
-                     ON UPDATE CASCADE 
-                     ON DELETE CASCADE 
-);
-
+insert into employees (department_id, first_name, last_name, email, phone_number, employed_at) values (20001, 'Terri', 'Aldersley', 'taldersley0@army.mil', '198-393-2278', '5/9/2023');
+insert into employees (department_id, first_name, last_name, email, phone_number, employed_at) values (20002, 'Gustav', 'Harte', 'gharte1@usa.gov', '448-500-3956', '2/3/2023');
+insert into employees (department_id, first_name, last_name, email, phone_number, employed_at) values (20003, 'Moina', 'Smy', 'msmy2@weather.com', '704-615-7509', '1/19/2023');
+insert into employees (department_id, first_name, last_name, email, phone_number, employed_at) values (20005, 'Mariette', 'Caltera', 'mcaltera4@cpanel.net', '515-969-8114', '12/26/2022');
+insert into employees (department_id, first_name, last_name, email, phone_number, employed_at) values (20006, 'Nicky', 'Attewill', 'nattewill5@ebay.com', '342-225-4473', '9/11/2023');
