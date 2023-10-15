60e1cd543f65b5ad72008c49c6d4afddcc0ac446
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-15 11:24:38 +0300

    Added sql file

--- sql_files/inserts_rings_table.sql
+++ sql_files/inserts_rings_table.sql
@@ -1,16 +1,37 @@
+CREATE TABLE rings(
+    id SERIAL PRIMARY KEY NOT NULL,
+    jewelry_type_id INTEGER NOT NULL,
+    image_url VARCHAR(200) NOT NULL,
+    ring_name VARCHAR(100) NOT NULL,
+    price DECIMAL(7, 2) NOT NULL,
+    metal_color VARCHAR(12) NOT NULL,
+    diamond_carat_weight VARCHAR(10) NOT NULL,
+    diamond_clarity VARCHAR(10) NOT NULL,
+    diamond_color VARCHAR(5) NOT NULL,
+    description TEXT NOT NULL,
+
+    CONSTRAINT fk_rings_jewelry_types
+                  FOREIGN KEY (jewelry_type_id)
+                  REFERENCES jewelry_types(id)
+                  ON UPDATE CASCADE
+                  ON DELETE CASCADE
+);
+
 INSERT INTO
     rings(
-          image_url, 
-          ring_name, 
-          price, 
-          metal_color, 
-          diamond_carat_weight, 
-          diamond_clarity, 
-          diamond_color, 
+          jewelry_type_id,
+          image_url,
+          ring_name,
+          price,
+          metal_color,
+          diamond_carat_weight,
+          diamond_clarity,
+          diamond_color,
           description
     )
 VALUES (
-        'https://res.cloudinary.com/deztgvefu/image/upload/v1697338358/uq3aoqmxaxczxbfg73p0.webp',
+        1,
+        'https://res.cloudinary.com/deztgvefu/image/upload/v1697350935/Rings/BUDDING_ROUND_BRILLIANT_DIAMOND_HALO_ENGAGEMENT_RING_s1ydsv.webp',
         'BUDDING ROUND BRILLIANT DIAMOND HALO ENGAGEMENT RING',
         19879.00,
         'ROSE GOLD',
