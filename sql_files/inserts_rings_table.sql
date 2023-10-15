5c20035f437802d11847464175f4d752fbedc154
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-15 07:11:00 +0300

    Added sql file

---
+++ sql_files/inserts_rings_table.sql
@@ -0,0 +1,21 @@
+INSERT INTO
+    rings(
+          image_url, 
+          ring_name, 
+          price, 
+          metal_color, 
+          diamond_carat_weight, 
+          diamond_clarity, 
+          diamond_color, 
+          description
+    )
+VALUES (
+        'https://res.cloudinary.com/deztgvefu/image/upload/v1697338358/uq3aoqmxaxczxbfg73p0.webp',
+        'BUDDING ROUND BRILLIANT DIAMOND HALO ENGAGEMENT RING',
+        19879.00,
+        'ROSE GOLD',
+        '1.75ctw',
+        'SI1-SI2',
+        'G-H',
+        'This stunning engagement ring features a round brilliant diamond with surrounded by a sparkling halo of marquise diamonds. Crafted to the highest standards and ethically sourced, it is the perfect ring to dazzle for any gift, proposal, or occasion. Its timeless design and exquisite craftsmanship will ensure an everlasting memory.'
+       );
