797929c1da8c9af9e364f17f1d6404bdab67541d
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-15 12:45:16 +0300

    Added sql file

--- sql_files/add_function_and_trigger.sql
+++ sql_files/add_function_and_trigger.sql
@@ -5,17 +5,38 @@ AS
 $$
 BEGIN
     INSERT INTO
-        jewelry_details(jewelry_id, jewelry_type_id, created_at, updated_at, deleted_at)
+        jewelry_inventory(jewelry_id, jewelry_type_id, quantity, created_at, updated_at, deleted_at)
     VALUES
-        (NEW.id, NEW.jewelry_type_id, DATE(NOW()), NULL, NULL);
+        (NEW.id, NEW.jewelry_type_id, DEFAULT, DATE(NOW()), NULL, NULL);
     RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER
-    tr_insert_new_entry
+    tr_insert_new_entry_rings
 AFTER INSERT ON
     rings
+FOR EACH ROW
+EXECUTE FUNCTION trigger_fn_insert_new_entry_into_jewelry_details();
+
+CREATE OR REPLACE TRIGGER
+    tr_insert_new_entry_earrings
+AFTER INSERT ON
+    earrings
+FOR EACH ROW
+EXECUTE FUNCTION trigger_fn_insert_new_entry_into_jewelry_details();
+
+CREATE OR REPLACE TRIGGER
+    tr_insert_new_entry_necklaces
+AFTER INSERT ON
+    necklaces
+FOR EACH ROW
+EXECUTE FUNCTION trigger_fn_insert_new_entry_into_jewelry_details();
+
+CREATE OR REPLACE TRIGGER
+    tr_insert_new_entry_bracelets
+AFTER INSERT ON
+    bracelets
 FOR EACH ROW
 EXECUTE FUNCTION trigger_fn_insert_new_entry_into_jewelry_details();
--- sql_files/jewelry_details.sql
+++ sql_files/jewelry_details.sql
@@ -1,7 +1,8 @@
 CREATE TABLE
-    jewelry_details(
+    jewelry_inventory(
         jewelry_id INTEGER NOT NULL,
         jewelry_type_id INTEGER NOT NULL,
+        quantity INTEGER DEFAULT 0 NOT NULL,
         created_at TIMESTAMP NOT NULL,
         updated_at TIMESTAMP,
         deleted_at TIMESTAMP
