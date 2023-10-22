2d7babd92f62300a4b19acb91afa19536e0290cf
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-22 20:17:31 +0300

    Added sql file

--- sql_files/jewelry_details.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-    jewelry_inventory(
-        jewelry_id INTEGER NOT NULL,
-        jewelry_type_id INTEGER NOT NULL,
-        quantity INTEGER DEFAULT 0 NOT NULL,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP,
-        deleted_at TIMESTAMP
-);
