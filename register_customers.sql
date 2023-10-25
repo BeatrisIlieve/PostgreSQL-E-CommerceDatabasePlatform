e9ae5a725f924b43ae034bd525040daf0cf155c9
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-25 19:56:55 +0300

    Added sql file

---
+++ register_customers.sql
@@ -0,0 +1,253 @@
+SELECT fn_register_user('beatris@icloud.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(1, 1, 3);
+CALL sp_complete_order(
+    1,
+    'Beatris',
+    'Ilieve',
+    '711-704-9768',
+    'Bulgaria',
+    'Sofia',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('peter@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(2, 5, 3);
+CALL sp_complete_order(
+    2,
+    'Peter',
+    'Harris',
+    '703-679-220',
+    'United Kingdom',
+    'London',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+
+SELECT fn_register_user('welch@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(3, 1, 2);
+CALL sp_complete_order(
+    3,
+    'Welch',
+    'Gorries',
+    '711-704-9768',
+    'Czechia',
+    'Brno',
+    'Some address',
+    200000.00,
+    'Amazon Pay'
+);
+
+SELECT fn_register_user('kellie@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(4, 1, 2);
+CALL sp_complete_order(
+    4,
+    'Kellie',
+    'Minihane',
+    '231-204-9598',
+    'Iceland',
+    'Banja Luka',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+
+SELECT fn_register_user('flora@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(5, 11, 3);
+CALL sp_complete_order(
+    5,
+    'Flora',
+    'Keating',
+    '203-679-4950',
+    'Denmark',
+    'Copenhagen',
+    'Some address',
+    200000.00,
+    'Stripe'
+);
+
+SELECT fn_register_user('james@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(6, 1, 2);
+CALL sp_complete_order(
+    6,
+    'James',
+    'Evans',
+    '303-689-4950',
+    'Finland',
+    'Helsinki',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('even@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(7, 12, 3);
+CALL sp_complete_order(
+    7,
+    'Even',
+    'Davis',
+    '503-689-4950',
+    'Malta',
+    'Valletta',
+    'Some address',
+    200000.00,
+    'Stripe'
+);
+
+SELECT fn_register_user('george@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(8, 10, 3);
+CALL sp_complete_order(
+    8,
+    'George',
+    'Thompson',
+    '993-689-4950',
+    'Austria',
+    'Vienna',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('john@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(9, 1, 3);
+CALL sp_complete_order(
+    9,
+    'John',
+    'Davis',
+    '493-689-4950',
+    'Portugal',
+    'Lisbon',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('hans@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(10, 2, 1);
+CALL sp_complete_order(
+    10,
+    'Hans',
+    'Davis',
+    '893-689-4950',
+    'Austria',
+    'Vienna',
+    'Some address',
+    200000.00,
+    'Amazon Pay'
+);
+
+SELECT fn_register_user('mary@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(11, 3, 1);
+CALL sp_complete_order(
+    11,
+    'Mary',
+    'Evens',
+    '893-689-4950',
+    'Austria',
+    'Vienna',
+    'Some address',
+    200000.00,
+    'Amazon Pay'
+);
+
+SELECT fn_register_user('daisy@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(12, 4, 3);
+CALL sp_complete_order(
+    12,
+    'Daisy',
+    'Davis',
+    '103-689-4950',
+    'Finland',
+    'Helsinki',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('victoria@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(13, 1, 3);
+CALL sp_complete_order(
+    13,
+    'Victoria',
+    'Nikolova',
+    '293-689-4950',
+    'Bulgaria',
+    'Plovdiv',
+    'Some address',
+    200000.00,
+    'Stripe'
+);
+
+SELECT fn_register_user('angel@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(14, 1, 4);
+CALL sp_complete_order(
+    14,
+    'Angel',
+    'Campbell',
+    '573-689-4998',
+    'Belgium',
+    'Brussels',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('bern@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(15, 7, 1);
+CALL sp_complete_order(
+    15,
+    'Bern',
+    'Wilson',
+    '983-689-4998',
+    'Sweden',
+    'Stockholm',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('mark@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(16, 6, 1);
+CALL sp_complete_order(
+    16,
+    'Mark',
+    'Davis',
+    '103-689-4998',
+    'Belgium',
+    'Brussels',
+    'Some address',
+    200000.00,
+    'PayPal'
+);
+
+SELECT fn_register_user('berry@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(17, 8, 2);
+CALL sp_complete_order(
+    17,
+    'Berry',
+    'Johnson',
+    '333-639-4998',
+    'Belgium',
+    'Brussels',
+    'Some address',
+    200000.00,
+    'Amazon Pay'
+);
+
+SELECT fn_register_user('garry@email.com', '#6hhh', '#6hhh');
+CALL sp_add_to_shopping_cart(18, 9, 2);
+CALL sp_complete_order(
+    18,
+    'Garry',
+    'Thompson',
+    '452-639-4998',
+    'Belgium',
+    'Brussels',
+    'Some address',
+    200000.00,
+    'Amazon Pay'
+);
