febfbd63fdba2a40dc13098fa6597bb4d614069b
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-25 19:50:14 +0300

    Added sql file

---
+++ insert_into_description.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+    description(
+        description
+)
+VALUES
+    (
+     'This stunning engagement ring features a round brilliant diamond with surrounded by a sparkling halo of marquise diamonds. Crafted to the highest standards and ethically sourced, it is the perfect ring to dazzle for any gift, proposal, or occasion. Its timeless design and exquisite craftsmanship will ensure an everlasting memory.'
+    ),
+    (
+    'These Diamond Halo Drop Earrings are perfect for any occasion. Featuring a halo of diamonds set around a drop earring design, they are beautifully crafted and certainly eye-catching. The diamonds provide exceptional sparkle and a luxurious feel that will make you shine.'
+    ),
+    (
+    'This classic Modern Diamond Bar Necklace is perfect for any occasion. Crafted with three shimmering diamonds and lustrous 14k gold, this chain is a timeless and elegant piece. The sparkle and shine of the diamonds will make you feel well dressed.'
+    ),
+    (
+     'This Gorgeous Pear Halo Bangle Bracelet will make a statement with two pear diamond centers and a halo of shimmering stones. Crafted from 14k gold, this beautiful bracelet is sure to make you shine.'
+    );
