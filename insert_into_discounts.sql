d6c78618d31f8e915da07baeccc36afefb5bc962
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-25 19:53:27 +0300

    Added sql file

---
+++ insert_into_discounts.sql
@@ -0,0 +1,31 @@
+CALL sp_insert_percent_into_discounts(
+    'merchandising_staff_user_first',
+    'merchandising_password_first',
+    '10002',
+    30001,
+    1,
+    0.10);
+
+CALL sp_insert_percent_into_discounts(
+    'merchandising_staff_user_first',
+    'merchandising_password_first',
+    '10002',
+    40001,
+    1,
+    0.15);
+
+CALL sp_insert_percent_into_discounts(
+    'merchandising_staff_user_first',
+    'merchandising_password_first',
+    '10002',
+    50001,
+    1,
+    0.20);
+
+CALL sp_insert_percent_into_discounts(
+    'merchandising_staff_user_first',
+    'merchandising_password_first',
+    '10002',
+    60001,
+    1,
+    0.25);
