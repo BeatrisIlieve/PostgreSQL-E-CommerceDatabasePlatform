3eb74dcae9aef60e80890d7d4e22737adc5d01a5
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-25 19:43:27 +0300

    Added sql file

---
+++ insert_into_staff_users.sql
@@ -0,0 +1,26 @@
+INSERT INTO
+    staff_users(
+            staff_user_role,
+            staff_user_password
+                )
+VALUES
+    (
+     'super_staff_user',
+     'super_staff_user_password'
+     ),
+    (
+     'merchandising_staff_user_first',
+     'merchandising_password_first'
+     ),
+    (
+     'merchandising_staff_user_second',
+     'merchandising_password_second'
+     ),
+    (
+     'inventory_staff_user_first',
+     'inventory_password_first'
+     ),
+    (
+     'inventory_staff_user_second',
+     'inventory_password_second'
+     );
