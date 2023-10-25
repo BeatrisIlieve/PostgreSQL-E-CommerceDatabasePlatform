c430e80e811f22da631e992137c0d047b54ec20a
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-25 19:46:55 +0300

    Added sql file

---
+++ insert_into_jewelry_name.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+    jewelry_name(
+        name
+)
+VALUES
+    (
+    'BUDDING ROUND BRILLIANT DIAMOND HALO ENGAGEMENT RING'
+    ),
+    (
+    'DIAMOND HALO DROP EARRING'
+    ),
+    (
+    'ROUND DIAMOND HALO PENDANT'
+    ),
+    (
+     'OVAL DIAMOND DETAIL BANGLE'
+    );
