c67bd2e0aa8f2a9e1ffc45c0596e01991ccb6241
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-25 19:52:38 +0300

    Added sql file

---
+++ insert_into_inventory.sql
@@ -0,0 +1,107 @@
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    30001,
+    1,
+    23);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_second',
+    'inventory_password_second',
+    '10005',
+    NULL,
+    30001,
+    2,
+    7);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    30001,
+    3,
+    5);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    40001,
+    1,
+    9);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_second',
+    'inventory_password_second',
+    '10005',
+    NULL,
+    40001,
+    2,
+    7);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    40001,
+    3,
+    5);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    50001,
+    1,
+    9);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_second',
+    'inventory_password_second',
+    '10005',
+    NULL,
+    50001,
+    2,
+    7);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    50001,
+    3,
+    5);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    60001,
+    1,
+    9);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_second',
+    'inventory_password_second',
+    '10005',
+    NULL,
+    60001,
+    2,
+    7);
+
+CALL sp_add_quantity_into_inventory(
+    'inventory_staff_user_first',
+    'inventory_password_first',
+    '10004',
+    NULL,
+    60001,
+    3,
+    5);
