b445e371d916590895caf1856db00bd5157d121c
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-25 19:51:31 +0300

    Added sql file

---
+++ insert_into_jewelries.sql
@@ -0,0 +1,192 @@
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_first',
+    'merchandising_password_first',
+    '10002',
+    30001,
+    1,
+    1,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698161350/Rings/BUDDING_ROUND_BRILLIANT_DIAMOND_HALO_ENGAGEMENT_RING_ROSE_watsbc.webp',
+    19879.00,
+    1,
+    1,
+    3,
+    2,
+    1
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_first',
+    'merchandising_password_first',
+    '10002',
+    30001,
+    1,
+    1,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698161350/Rings/BUDDING_ROUND_BRILLIANT_DIAMOND_HALO_ENGAGEMENT_RING_YELLOW_vvqx9m.webp',
+    19879.00,
+    2,
+    1,
+    3,
+    2,
+    1
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_first',
+    'merchandising_password_first',
+    '10002',
+    30001,
+    1,
+    1,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698161350/Rings/BUDDING_ROUND_BRILLIANT_DIAMOND_HALO_ENGAGEMENT_RING_WHITE_cuu8dz.webp',
+    19879.00,
+    3,
+    1,
+    3,
+    2,
+    1
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    40001,
+    2,
+    2,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698162227/Rings/DIAMOND_HALO_DROP_EARRING_ROSE_sf6dfj.webp',
+    22749.00,
+    1,
+    2,
+    1,
+    3,
+    2
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    40001,
+    2,
+    2,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698162227/Rings/DIAMOND_HALO_DROP_EARRING_YELLOW_mdwtqu.webp',
+    22749.00,
+    2,
+    2,
+    1,
+    3,
+    2
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    40001,
+    2,
+    2,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698162228/Rings/DIAMOND_HALO_DROP_EARRING_WHITE_kjf8zt.webp',
+    22749.00,
+    3,
+    2,
+    1,
+    3,
+    2
+);
+
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    50001,
+    3,
+    3,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698163016/Rings/ROUND_DIAMOND_HALO_PENDANT_ROSE_h7njim.webp',
+    17599.00,
+    1,
+    3,
+    3,
+    3,
+    3
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    50001,
+    3,
+    3,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698163016/Rings/ROUND_DIAMOND_HALO_PENDANT_YELLOW_e6srd6.webp',
+    17599.00,
+    2,
+    3,
+    3,
+    3,
+    3
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    50001,
+    3,
+    3,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698163016/Rings/ROUND_DIAMOND_HALO_PENDANT_WHITE_tilgrt.webp',
+    17599.00,
+    3,
+    3,
+    3,
+    3,
+    3
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    60001,
+    4,
+    4,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698163533/Rings/OVAL_DIAMOND_DETAIL_BANGLE_ROSE_y2tqfd.webp',
+    15743.00,
+    1,
+    3,
+    2,
+    3,
+    4
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    60001,
+    4,
+    4,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698163532/Rings/OVAL_DIAMOND_DETAIL_BANGLE_YELLOW_zrny35.webp',
+    15743.00,
+    2,
+    3,
+    2,
+    3,
+    4
+);
+
+CALL sp_insert_jewelry_into_jewelries(
+    'merchandising_staff_user_second',
+    'merchandising_password_second',
+    '10003',
+    60001,
+    4,
+    4,
+    'https://res.cloudinary.com/deztgvefu/image/upload/v1698163533/Rings/OVAL_DIAMOND_DETAIL_BANGLE_WHITE_i05ggo.webp',
+    15743.00,
+    3,
+    3,
+    2,
+    3,
+    4
+);
